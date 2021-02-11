01ed9da1dfb07ec7cb94569b1c78786f7c3db2bf
Javi <javier.cortes.ortega@gmail.com>
2021-02-11 16:16:39 +0100

    Remove old dotfiles & install ag

--- dotfiles/.i3/autolock.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec xautolock -detectsleep -time 3 -locker "exec i3lock -t -i $(find ~/.config/wallpapers/ | shuf -n1)" -corners ---- -cornersize 30 -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'" &
